a69b1d7f48dcc43ed87df9be09847acf11e1b69c
victorbrandaoa <victor.andrade@ccc.ufcg.edu.br>
2022-02-23 22:08:26 -0300

    removing template js files and change the directory structure

--- src/app.module.js
+++ src/app.module.js
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './database.module';
 import config from './config';
 
@@ -12,7 +10,7 @@ import config from './config';
     }),
     DatabaseModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
