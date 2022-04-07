577c4575f882a2fd309afa753cd30c5fd57f04cd
victorbrandaoa <victor.andrade@ccc.ufcg.edu.br>
2022-04-06 21:58:43 -0300

    add username validation to the PUT route

--- src/users/users.service.js
+++ src/users/users.service.js
@@ -2,7 +2,8 @@ import {
   Injectable,
   Dependencies,
   NotFoundException,
-  ConflictException
+  ConflictException,
+  BadRequestException
 } from '@nestjs/common';
 import { getModelToken } from '@nestjs/mongoose';
 import { User } from './user.schema';
@@ -48,10 +49,20 @@ export class UsersService {
   async putUser(username, user) {
     await this.getUserByUsername(username);
 
+    const { username: newUsername } = user;
+    if (newUsername !== username) {
+      const userExists = await this.existsUser(newUsername);
+      if (userExists) {
+        throw new BadRequestException(`Invalid new username. There's another user with the username ${newUsername}`);
+      }
+    }
+
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(user.password, salt);
 
-    return this.userModel.updateOne({ username }, { ...user, password: hashedPassword });
+    await this.userModel.updateOne({ username }, { ...user, password: hashedPassword });
+
+    return this.getUserByUsername(newUsername);
   }
 
   async deleteUser(username) {
